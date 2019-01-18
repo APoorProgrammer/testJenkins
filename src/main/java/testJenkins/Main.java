a690f2102207c7423b48886b25a8d10ae8511318
apoorprogrammer <apoorprogrammer@gmail.com>
2019-01-18 11:54:49 +0100

    Rebuild project?

--- src/main/java/testJenkins/Main.java
+++ src/main/java/testJenkins/Main.java
@@ -3,7 +3,8 @@ package testJenkins;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world Again!!!");
 	}
 
 }
